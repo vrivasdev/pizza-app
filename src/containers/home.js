f9e46cac0cd44d4464c5190900da82fd15dbdac5
Victor Rivas <victor.rivas.sistemas@gmail.com>
2020-04-29 20:42:34 -0400

    Service main structure

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react'
 import HomeLayout from '../components/home-layout'
+import CartService from '../services/cartService';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.service = new CartService();
+    }
+    componentDidMount () {
+        this.service.getPizzas();
+    }
     render() {
         return(
             <HomeLayout data={this.props.data}/>
---
+++ src/services/cartService.js
@@ -0,0 +1,17 @@
+
+class CartService {
+    getPizzas() {
+        console.log('pizzaaa')
+        /*Promise((resolve, reject) => {
+            resolve([{'test':'test'}]);
+        });*/
+    }
+    saveOrder() {
+        // fetch
+    }
+    saveCart() {
+        // fetch
+    }
+}
+
+export default CartService;
